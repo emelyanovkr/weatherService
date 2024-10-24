f190507e205d019acf310ed3569c611b470e32f9
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-10-24 14:37:07 +0300

    init repo

---
+++ src/test/java/emv/weather/WeatherServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package emv.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
